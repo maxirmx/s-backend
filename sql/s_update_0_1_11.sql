01bd0aaef818478d32a0579438b6d90905839692
Maxim [maxirmx] Samsonov <m.samsonov@computer.org>
2023-09-17 20:48:42 +0300

    Changed shipment number size to 128

--- sql/s_update_0_1_11.sql
+++ sql/s_update_0_1_11.sql
@@ -1,4 +1,5 @@
 START TRANSACTION;
 INSERT INTO `versions`(`version`, `date`) VALUES ('0.1.11','2023-09-12');
-INSERT INTO `versions`(`version`, `date`) VALUES ('0.1.12','2023-09-15');
+INSERT INTO `versions`(`version`, `date`) VALUES ('0.1.12','2023-09-17');
+ALTER TABLE `shipments` CHANGE `number` `number` VARCHAR(128) CHARACTER SET utf8 COLLATE utf8_unicode_520_ci NOT NULL;
 COMMIT;
