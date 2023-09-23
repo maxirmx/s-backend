28dc2fa43fa23cfce1d91319ea3850b302af0e26
Maxim [maxirmx] Samsonov <m.samsonov@computer.org>
2023-09-24 00:41:30 +0300

    Version 0.2.0 (2)

--- sql/s_update_0_2_0.sql
+++ sql/s_update_0_2_0.sql
@@ -1,5 +1,6 @@
 START TRANSACTION;
 INSERT INTO `versions`(`version`, `date`) VALUES ('0.1.13','2023-09-20');
+INSERT INTO `versions`(`version`, `date`) VALUES ('0.2.0','2023-09-24');
 CREATE TABLE `user_org_mappings` ( `id` INT NOT NULL AUTO_INCREMENT , `userId` INT NOT NULL , `orgId` INT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 ALTER TABLE `user_org_mappings` ADD INDEX `mapping_user_idx` (`userId`);
 ALTER TABLE `user_org_mappings` ADD INDEX `mapping_org_idx` (`orgId`);
