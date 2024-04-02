842c2ed9aac3c7c13bcfec885552a2721c7767a7
Maxim [maxirmx] Samsonov <m.samsonov@computer.org>
2024-04-03 00:21:37 +0300

    Saved backend config template (2)

---
+++ php/inc/config.php
@@ -0,0 +1,39 @@
+<?php
+/**
+*  Copyright (C) 2024 Maxim [maxirmx] Samsonov (www.sw.consulting)
+*  All rights reserved.
+*  This file is a part of s-tracker applcation
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*  1. Redistributions of source code must retain the above copyright
+*  notice, this list of conditions and the following disclaimer.
+*  2. Redistributions in binary form must reproduce the above copyright
+*  notice, this list of conditions and the following disclaimer in the
+*  documentation and/or other materials provided with the distribution.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+*  TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+*  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS
+*  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+*  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+*  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+*  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+*  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+*  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*/
+
+define("DB_HOST", "db.host");
+define("DB_USERNAME", "db.username");
+define("DB_PASSWORD", "db.password");
+define("DB_DATABASE_NAME", "db.database");
+define("JWT_SECRET", "jwt.secret");
+define("JWT_EXPIRE", 60*60*4);
+define("CORS_ALLOW", true);
+define("SERVICE_EMAIL", "service.email");
+define("FINAL_STATUS", "15");
+define("ARCHIEVE_THRESHOLD", "3");
+?>
